28446a2e8ea1cc2c9a66b73593498aee27c3ccb7
HenryZhuHR <296506195@qq.com>
2023-04-13 15:46:17 +0800

    🔄 workflow: server

---
+++ scripts/download.sh
@@ -0,0 +1,6 @@
+mkdir -p libs
+cd libs
+git clone https://github.com/CompVis/taming-transformers.git taming-transformers\
+    -b master
+git clone https://github.com/openai/CLIP.git clip \
+    -b main
