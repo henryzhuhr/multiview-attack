40d2fc941549587ac8885fa8091fd4af4cd2d920
HenryZhuHR <296506195@qq.com>
2023-04-24 15:08:57 +0800

    🚑 hotfix:  fix error tensor.contiguous()
    avoid `RuntimeError: input must be contiguous`: see [blog](https://blog.csdn.net/kdongyi/article/details/108180250)

--- tsgan/models/op/fused_act.py
+++ tsgan/models/op/fused_act.py
@@ -6,7 +6,6 @@ from torch.nn import functional as F
 from torch.autograd import Function
 from torch.utils.cpp_extension import load
 
-
 module_path = os.path.dirname(__file__)
 fused = load(
     "fused",
@@ -26,9 +25,7 @@ class FusedLeakyReLUFunctionBackward(Function):
 
         empty = grad_output.new_empty(0)
 
-        grad_input = fused.fused_bias_act(
-            grad_output.contiguous(), empty, out, 3, 1, negative_slope, scale
-        )
+        grad_input = fused.fused_bias_act(grad_output.contiguous(), empty, out, 3, 1, negative_slope, scale)
 
         dim = [0]
 
@@ -46,8 +43,16 @@ class FusedLeakyReLUFunctionBackward(Function):
     @staticmethod
     def backward(ctx, gradgrad_input, gradgrad_bias):
         out, = ctx.saved_tensors
+
         gradgrad_out = fused.fused_bias_act(
-            gradgrad_input, gradgrad_bias, out, 3, 1, ctx.negative_slope, ctx.scale
+            gradgrad_input.contiguous(
+            ),                               # avoid `RuntimeError: input must be contiguous`: https://blog.csdn.net/kdongyi/article/details/108180250
+            gradgrad_bias,
+            out,
+            3,
+            1,
+            ctx.negative_slope,
+            ctx.scale,
         )
 
         return gradgrad_out, None, None, None, None
@@ -85,7 +90,7 @@ class FusedLeakyReLUFunction(Function):
 
 
 class FusedLeakyReLU(nn.Module):
-    def __init__(self, channel, bias=True, negative_slope=0.2, scale=2 ** 0.5):
+    def __init__(self, channel, bias=True, negative_slope=0.2, scale=2**0.5):
         super().__init__()
 
         if bias:
@@ -101,16 +106,11 @@ class FusedLeakyReLU(nn.Module):
         return fused_leaky_relu(input, self.bias, self.negative_slope, self.scale)
 
 
-def fused_leaky_relu(input, bias=None, negative_slope=0.2, scale=2 ** 0.5):
+def fused_leaky_relu(input, bias=None, negative_slope=0.2, scale=2**0.5):
     if input.device.type == "cpu":
         if bias is not None:
             rest_dim = [1] * (input.ndim - bias.ndim - 1)
-            return (
-                F.leaky_relu(
-                    input + bias.view(1, bias.shape[0], *rest_dim), negative_slope=0.2
-                )
-                * scale
-            )
+            return (F.leaky_relu(input + bias.view(1, bias.shape[0], *rest_dim), negative_slope=0.2) * scale)
 
         else:
             return F.leaky_relu(input, negative_slope=0.2) * scale
