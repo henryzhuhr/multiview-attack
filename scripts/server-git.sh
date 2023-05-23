341339d2fcc2f48627d445d7955a86c7e51f0589
HenryZhuHR <296506195@qq.com>
2023-05-23 12:16:37 +0800

    server about

---
+++ scripts/server-git.sh
@@ -0,0 +1,7 @@
+SOURCE_DIR="zhr@$N4090_SERVER:~/Project/diffusion-attack/.git"
+TARGET_DIR="./"
+mkdir -p $TARGET_DIR
+echo "$SOURCE_DIR -> $TARGET_DIR"
+
+rsync -av $SOURCE_DIR $TARGET_DIR \
+    --link-dest $TARGET_DIR
